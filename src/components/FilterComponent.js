1865bcdcd1d5fe91c0486c646a8c437d82695e7e
achilan <anthony.chilanp@gmail.com>
2022-09-27 18:47:17 -0500

    er

--- src/components/FilterComponent.js
+++ src/components/FilterComponent.js
@@ -6,13 +6,12 @@ export const FilterComponent = (data) => {
   return (
     <InputGroup className="mb-1">
       <Form.Control
-        placeholder="Recipient's username"
-        aria-label="Recipient's username"
+        placeholder="Search"
+        aria-label="Search"
         aria-describedby="basic-addon2"
         value={data.filterText}
         onChange={(e) => {
            data.onFilter(e);
-           console.log(data)
         }}
       />
       <Button onClick={data.onClear} variant="outline-secondary" id="button-addon2">
