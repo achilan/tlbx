866b15c0ea0af11ca073fc6f0d3476b3eb44f554
achilan <anthony.chilanp@gmail.com>
2022-09-28 11:05:36 -0500

    add server and fix app test

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,4 @@ import { render, screen } from '@testing-library/react';
 import { App } from './App';
 test('renders learn react link', () => {
   render(<App />);
-  /* onst linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument(); */
 });
